f87af1a76a1c8db6cbeb6336d591a3e831903a64
Peeko <omarock27@gmail.com>
2018-10-11 21:16:45 -0700

    Cambios visuales menores
    Cambio del titulo con touchableOpacity entre otras cosas

--- my-app/App.js
+++ my-app/App.js
@@ -1,6 +1,6 @@
 //import stuff
 import React from 'react';
-import {View,Text,TextInput, Button} from 'react-native';
+import {View,Text,TextInput, Button, TouchableOpacity} from 'react-native';
 
 //create stuff
 class App extends React.Component{
@@ -14,44 +14,74 @@ class App extends React.Component{
     arr.push(newTodo);
     this.setState({todo: arr, text: ""});
   }
+  deleteTodo = (t) =>{
+    var arr = this.state.todo;
+    var pos = arr.indexOf(t);
+    arr.splice(pos, 1);
+    this.setState({todo: arr});
+  }
   renderTodos = () =>{
     return this.state.todo.map(t=>{
       return(
-        <Text key={t}>{t}</Text>
+        <TouchableOpacity key={t}>
+        <Text
+          style={styles.todo}      
+          onPress={()=>{this.deleteTodo(t)}}
+          >{t}</Text>
+          </TouchableOpacity>
       )
     })
   }
   render(){
     return(
-      <View style={styles.viewStyle}>
-        <Text>Pequeña app Todo</Text>
-        <TextInput
-        style={styles.inputStyle}
-        onChangeText={(text)=>this.setState({text})}
-        value={this.state.text}
-        />
-        <Button
-        title="Add Todo"
-        color="skyblue"
-        onPress={this.addTodo}
-        />
-        {this.renderTodos()}
+      <View style={styles.wholeStyle}>
+        <View style={styles.viewStyle}>
+          <TouchableOpacity>
+          <Text style={styles.header}>App De Notas</Text>
+          </TouchableOpacity>        
+          <TextInput
+          style={styles.inputStyle}
+          onChangeText={(text)=>this.setState({text})}
+          value={this.state.text}
+          />
+          <Button
+          title="Agregar tarea"
+          color="#03A9F4"
+          onPress={this.addTodo}
+          />
+          <View style={{marginTop: 100}}/>
+          {this.renderTodos()}
+        </View>
       </View>
     )
   }
 }
 
 const styles = {
+  wholeStyle: {
+    backgroundColor: "#0288D1",
+    flex: 1
+  },
   viewStyle:{
-  flex: 1,
+  marginTop: 30,  
   alignItems: 'center',
-  justifyContent: 'center'
+  justifyContent: 'center',
+  margin: 10
   },
   inputStyle:{
     height:40,
     width: 300,
-    borderColor: "powderblue",
+    borderColor: "white",
     borderWidth: 1 
+  },
+  header:{
+    fontSize: 30,
+    color: 'white',
+    fontWeight: 'bold'
+  },
+  todo: {
+    fontSize: 24,
+    color: 'white'
   }
 }
 
